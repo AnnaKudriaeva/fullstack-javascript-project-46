d2e7efdf9565a5828156ff49badc8f670b8e63fe
Anna Kudriaeva <anna.kudriaeva@gmail.com>
2024-08-09 11:06:55 +0300

    edit tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,62 +4,52 @@ import fs from 'fs';
 import generateDiff from '../lib/gendiff.js';
 import parseFile from '../lib/parsers.js';
 
+const runTest = (file1, file2, expectedFile, format) => {
+  const data1 = parseFile(file1);
+  const data2 = parseFile(file2);
+  const expectedOutput = fs.readFileSync(expectedFile, 'utf-8').trim();
+  const diff = generateDiff(data1, data2, format);
+  expect(diff).toBe(expectedOutput);
+};
+
 test('genDiff JSON', () => {
   const json1 = path.resolve('fixtures/file1.json');
   const json2 = path.resolve('fixtures/file2.json');
-  const data1 = parseFile(json1);
-  const data2 = parseFile(json2);
-  const expectedOutput = fs.readFileSync(path.resolve('fixtures/expected_output.txt'), 'utf-8').trim();
-  const diff = generateDiff(data1, data2, 'stylish');
-  expect(diff).toBe(expectedOutput);
+  const expectedOutput = path.resolve('fixtures/expected_output.txt');
+  runTest(json1, json2, expectedOutput, 'stylish');
 });
 
 test('genDiff YAML', () => {
   const yml1 = path.resolve('fixtures/file1.yml');
   const yml2 = path.resolve('fixtures/file2.yml');
-  const data1 = parseFile(yml1);
-  const data2 = parseFile(yml2);
-  const expectedOutput = fs.readFileSync(path.resolve('fixtures/expected_output.txt'), 'utf-8').trim();
-  const diff = generateDiff(data1, data2, 'stylish');
-  expect(diff).toBe(expectedOutput);
+  const expectedOutput = path.resolve('fixtures/expected_output.txt');
+  runTest(yml1, yml2, expectedOutput, 'stylish');
 });
 
 test('genDiff JSON plain', () => {
   const json1 = path.resolve('fixtures/file1.json');
   const json2 = path.resolve('fixtures/file2.json');
-  const data1 = parseFile(json1);
-  const data2 = parseFile(json2);
-  const expectedOutput = fs.readFileSync(path.resolve('fixtures/expected_output_plain.txt'), 'utf-8').trim();
-  const diff = generateDiff(data1, data2, 'plain');
-  expect(diff).toBe(expectedOutput);
+  const expectedOutput = path.resolve('fixtures/expected_output_plain.txt');
+  runTest(json1, json2, expectedOutput, 'plain');
 });
 
 test('genDiff YAML plain', () => {
   const yml1 = path.resolve('fixtures/file1.yml');
   const yml2 = path.resolve('fixtures/file2.yml');
-  const data1 = parseFile(yml1);
-  const data2 = parseFile(yml2);
-  const expectedOutput = fs.readFileSync(path.resolve('fixtures/expected_output_plain.txt'), 'utf-8').trim();
-  const diff = generateDiff(data1, data2, 'plain');
-  expect(diff).toBe(expectedOutput);
+  const expectedOutput = path.resolve('fixtures/expected_output_plain.txt');
+  runTest(yml1, yml2, expectedOutput, 'plain');
 });
 
 test('genDiff JSON json', () => {
   const json1 = path.resolve('fixtures/file1.json');
   const json2 = path.resolve('fixtures/file2.json');
-  const data1 = parseFile(json1);
-  const data2 = parseFile(json2);
-  const expectedOutput = fs.readFileSync(path.resolve('fixtures/expected_output_json.json'), 'utf-8').trim();
-  const diff = generateDiff(data1, data2, 'json');
-  expect(diff).toBe(expectedOutput);
+  const expectedOutput = path.resolve('fixtures/expected_output_json.json');
+  runTest(json1, json2, expectedOutput, 'json');
 });
 
 test('genDiff YAML json', () => {
   const yml1 = path.resolve('fixtures/file1.yml');
   const yml2 = path.resolve('fixtures/file2.yml');
-  const data1 = parseFile(yml1);
-  const data2 = parseFile(yml2);
-  const expectedOutput = fs.readFileSync(path.resolve('fixtures/expected_output_json.json'), 'utf-8').trim();
-  const diff = generateDiff(data1, data2, 'json');
-  expect(diff).toBe(expectedOutput);
+  const expectedOutput = path.resolve('fixtures/expected_output_json.json');
+  runTest(yml1, yml2, expectedOutput, 'json');
 });
